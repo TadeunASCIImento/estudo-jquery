2aa6a687d6a82da8e24c6ec032f2f340acfff805
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-12-17 03:15:21 -0300

    Usando a função text e split para contar as palavras de uma frase..

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,12 @@
+// exibe o texto da frase no console.
 const paragrafo2 = $(".paragrafo2").text();
 console.log(paragrafo2);
+
+// exibe a quantidade de palavras da frase no console.
+let quantidade  = paragrafo2.split(" ").length;
+console.log(quantidade);
+
+let tamanho = $(".tamanho");
+tamanho.text(quantidade);
+
+
