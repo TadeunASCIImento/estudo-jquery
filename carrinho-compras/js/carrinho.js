d0a401a6e7e419ead1f6552e8472d08166057698
Tadeu Nascimento <47820260+TadeunASCIImento@users.noreply.github.com>
2021-11-30 16:21:45 -0300

    [main] Melhorando a função de cálculo do valor total da compra.

--- carrinho-compras/js/carrinho.js
+++ carrinho-compras/js/carrinho.js
@@ -2,15 +2,17 @@ function getValorTotalCarrinho(){
     let total = 0;
     const itens = document.querySelectorAll(".item");
     for (let i = 0,len = itens.length; i < len;i++){
-        const quantidade = itens[i].querySelector(".quantidade").value;
-        if(quantidade < 0){
+         const quantidade = itens[i].querySelector(".quantidade").value;
+        if (quantidade < 0){
             itens[i].querySelector(".quantidade").value = '';
-        } else {
+        }
+        else {
             const preco = formatToNumber(itens[i].querySelector(".preco").innerText);
-            const _total = formatMoney(total += quantidade * parseFloat(preco));                                                                                 
+            const subtotal = quantidade * preco; 
             let totalizador = document.querySelector(".totalizador");
-            totalizador.innerText = '';
-            totalizador.innerText = 'Total da Compra:'.concat(_total);
+            clearTotalizador(totalizador);            
+            total +=subtotal;
+            totalizador.innerText = 'Total da Compra:'.concat(formatMoney(total));                                                                                 
         }
     }
 }
@@ -22,3 +24,15 @@ function formatMoney(string){
 function formatToNumber(string){
     return string.split("R$").join('').replace(',','.');
 }
+
+function clearTotalizador(totalizador){
+    totalizador.innerText = '';
+}
+
+function onLoadPage(){
+    const quantidades = document.querySelectorAll('.quantidade');
+    quantidades.forEach(e => {
+        e.onchange = getValorTotalCarrinho;
+    });
+}
+window.onload = onLoadPage;
