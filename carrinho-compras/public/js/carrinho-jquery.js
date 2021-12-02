1e2f8f71422207c452cc646eda4c2cee01128caf
Tadeu Nascimento <47820260+TadeunASCIImento@users.noreply.github.com>
2021-12-02 18:20:27 -0300

    [jquery-0001] Reescrevendo a função de cálculo total da compra usando a JQuery.

--- carrinho-compras/public/js/carrinho-jquery.js
+++ carrinho-compras/public/js/carrinho-jquery.js
@@ -1,5 +1,28 @@
 function getValorTotalCarrinho(){
-    let itens = $(".item");
-    console.log($('.preco')[0].textContent);
+    let total = 0;
+    [ ...$(".item") ].forEach(item => {
+        if ($('.quantidade',item).val() < 0){
+            $('.quantidade', item).val('');
+        } else {
+            $(".totalizador").val('');
+            total += $('.quantidade',item).val() * convert($('.preco',item).text());
+            $('.totalizador').text('Total da Compra:'.concat(money(total, 'BRL')));                                                                                 
+        }
+    });
 }
-getValorTotalCarrinho();
+
+function money(value, currency){
+    return value.toLocaleString(currency,{style:'currency', currency: currency});
+}
+
+function convert(string){
+    return string.split("R$").join('').replace(',','.');
+}
+
+function onLoadPage(){
+    const quantidades = document.querySelectorAll('.quantidade');
+    quantidades.forEach(e => {
+        e.onchange = getValorTotalCarrinho;
+    });
+}
+window.onload = onLoadPage;
