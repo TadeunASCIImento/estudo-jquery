88ceff536f92e5c9e54954d61de16ab7a11d5317
Tadeu Nascimento <47820260+TadeunASCIImento@users.noreply.github.com>
2021-12-10 23:04:57 -0300

    [jquery-0001] Ajustes

--- lista-tarefas/js/tarefas.js
+++ lista-tarefas/js/tarefas.js
@@ -1,4 +1,18 @@
-$('#tarefa').keydown((event) => {
-   console.log(event.which,String.fromCharCode(event.which));
-});
-    
+// Cria uma nova tarefa na lista
+function createTask(){
+    $('#tarefa').off('keydown').keydown((event) => {
+        if (event.which === 13){
+            if($('#tarefa').val() !== ''){
+              const tarefa    =  $('#tarefa').val();
+              const row       =  $('<tr />').addClass('row-tarefa');
+              const dataTask  =  $('<td />').text(tarefa);
+              const link      =  $('<a />' , {href:"#"});
+              const dataIcone =  $('<td />').append(link);
+              const imgIcone  =  $('<img />').attr({src:'img/trash_icon.png'});
+              link.append(imgIcone);
+              $('.tbody').append(row.append(dataTask).append(dataIcone));          
+            }
+        }
+    }); 
+}
+createTask();
